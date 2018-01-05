536f47bb2ae99eef822d34a594d7355ea40da194
Tim Work Laptop <tmstani23@gmail.com>
2018-01-05 16:18:59 -0600

    Update 2
    Created variables for user input to hour,min,second.  Created variables that hold hour,min,second values.
    Created interval loop that calls a function every second.  Created function that sets different hour, minute and second variables.  Created conditionals for updating the time variables.  Created a function that stops the timer when called.

--- pomodoro.js
+++ pomodoro.js
@@ -1,14 +1,42 @@
+//Create a timer that displays seconds counting down from 60 and stopping at 0
+let inputHour = 1;
+let inputMin = 1;
+let inputSec = 5;
 
-let today = new Date();
+let setHour = inputHour;
+let setMin = inputMin;
+let setSec = inputSec;
 
-let initialSec = today.getSeconds();
-console.log(initialSec);
+//Timer that calls displayTime function every seconds;
+let secTimer = setInterval(displayTime, 1000);
 
-//today.setHours(0, 0, 0, 0);
-//console.log(today); // still today, now 00:00:00 sharp.
-setTimeout(displaySeconds, 3000);
 
-function displaySeconds() {
-    let seconds = today.getSeconds();
-    console.log(seconds);
+
+function displayTime() {
+    
+    if (setHour == 0 && setSec == 0 && setMin == 0) {
+        return endTimer();
+    }
+    else if (setHour > 0 && setMin == 0 && setSec == 0) {
+        setHour = setHour - 1;
+        setMin = 1;
+        setSec = 5;
+    }
+   
+    else if (setSec == 0) {
+        setMin = setMin - 1;
+        setSec = 10;
+    }
+    
+    setSec = setSec - 1
+    
+    console.log(setHour + "hour");
+    console.log(setMin + "min");
+    console.log(setSec + "sec");
+    
 }
+function endTimer() {
+    clearInterval(secTimer);
+}
+
+
