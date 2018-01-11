422fb784e0dbbb18351b60ac1229ef3563f43328
Tim Work Laptop <tmstani23@gmail.com>
2018-01-11 15:37:55 -0600

    Update 5
    Added html input forms for hours,min,seconds.  Added function to take the inputs and save them to global variables.

--- pomodoro.js
+++ pomodoro.js
@@ -1,7 +1,10 @@
 //Create a timer that displays seconds counting down from 60 and stopping at 0
+// let inputHour = 0;
+// let inputMin = 25;
+// let inputSec = 60;
 let inputHour = 0;
-let inputMin = 1;
-let inputSec = 5;
+let inputMin = 25;
+let inputSec = 60;
 let secTimer = 0;
 let setHour = inputHour;
 let setMin = inputMin;
@@ -10,13 +13,27 @@ let setSec = inputSec;
 //let paused = false;
 //var secTimer = setInterval(function(){ calcTime() }, 1000);
 
+//function that takes user inputs and saves them to global variables
+function getTInputs() {
+    //get number input and save to globals
+    inputHour = document.getElementById("hrInput").value;
+    inputMin = document.getElementById("minInput").value;
+    inputSec = document.getElementById("secInput").value;
+    //display time to screen:
+    result = inputHour + `:`+ inputMin + `:` + inputSec;
+    displayTime(result);
+    console.log(inputMin);
+
+
+    //check if input is a number if not throw an error
+    //check if timer is running if yes throw an error
+
+}
+
+
 //Timer that calls displayTime function every seconds;
 function startTimer() {
-    // if(secTimer == true) {
-    //     console.log("istrue");
-    //     stopTimer();
-    //     return;
-    // }
+   
     console.log(secTimer);
     if (secTimer >= 1) {
         //stopTimer();
@@ -39,26 +56,27 @@ function startTimer() {
 }
 
 function calcTime() {
+    
     let result = "";
     if (setHour == 0 && setSec == 0 && setMin == 0) {
         stopTimer();
     }
     else if (setHour > 0 && setMin == 0 && setSec == 0) {
         setHour = setHour - 1;
-        setMin = 1;
-        setSec = 5;
+        setMin = inputMin;
+        setSec = inputSec;
     }
    
     else if (setSec == 0) {
         setMin = setMin - 1;
-        setSec = 10;
+        setSec = inputSec;
     }
     
     if (!setSec == 0) {
         setSec = setSec - 1
     }
     
-    result = setHour + " " + setMin + " " + setSec;
+    result = setHour + `:`+ setMin + `:` + setSec;
     
     // console.log(setHour + "hour");
     // console.log(setMin + "min");
